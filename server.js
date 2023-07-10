bc9318936d3998cdce0c5ca2c3e9e6a58aa9e5b2
bhagyashri4137@gmail.com <bhagyashri4137@gmail.com>
2023-07-10 10:12:23 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors('*'))
+
+app.get('/', (request, response) => {
+  response.send('This is simple server')
+})
+
+app.listen(5000, '0.0.0.0', () => {
+  console.log('server started on port 5000')
+})
